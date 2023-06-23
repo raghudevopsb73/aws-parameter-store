010f2164e4ae6acdf8389e201186fec8a62fdc65
Raghu K <raghuk.vit@gmail.com>
2023-06-23 09:03:34 +0530

    Update aws-parameter-store/

--- main.tf
+++ main.tf
@@ -1,8 +1,9 @@
 resource "aws_ssm_parameter" "params" {
-  count = length(var.params)
-  name  = var.params[count.index].name
-  type  = var.params[count.index].type
-  value = var.params[count.index].value
+  count     = length(var.params)
+  name      = var.params[count.index].name
+  type      = var.params[count.index].type
+  value     = var.params[count.index].value
+  overwrite = true
 }
 
 variable "params" {
