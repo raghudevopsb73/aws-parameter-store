e0da721a327449df946ab164afec558a09dfe9e2
Raghu K <raghuk.vit@gmail.com>
2023-06-30 08:39:23 +0530

    Update aws-parameter-store/

--- main.tf
+++ main.tf
@@ -31,7 +31,7 @@ variable "params" {
     { name = "roboshop.dev.shipping.db_host", value = "mysql-dev.rdevopsb73.online", type = "String" },
     { name = "roboshop.dev.user.mongo", value = "true", type = "String" },
     { name = "roboshop.dev.user.redis_host", value = "redis-dev.rdevopsb73.online", type = "String" },
-    { name = "roboshop.dev.user.mongo_url", value = "mongodb://mongodb-dev.rdevopsb73.online:27017/users", type = "String" },
+    //    { name = "roboshop.dev.user.mongo_url", value = "mongodb://mongodb-dev.rdevopsb73.online:27017/users", type = "String" },
     { name = "roboshop.dev.mysql.username", value = "roboshop", type = "String" },
     { name = "roboshop.dev.docdb.username", value = "roboshop", type = "String" },
     { name = "roboshop.dev.rabbitmq.amqp_user", value = "roboshop", type = "String" },
